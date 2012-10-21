28adb774a358f4728b9e99da5322e7c8020676bb
Bryan <bryan@ubuntu.(none)>
2012-10-21 16:05:28 -0700

    rough jquery skeleton

--- Gemfile
+++ Gemfile
@@ -6,7 +6,6 @@ gem 'rails', '3.2.8'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3'
-gem 'mysql2'
 gem 'haml-rails'
 
 # Gems used only for assets and not required
--- app/assets/javascripts/reservation.js
+++ app/assets/javascripts/reservation.js
@@ -1,5 +1,52 @@
 /*globals _ */
+var template;
+var ajaxCalls = {};
+
+ajaxCalls.addNew = function(firstname, lastname, seats, notes) {
+  var data = { appointment: { first_name: firstname, last_name: lastname, seats: seats, notes: notes }};
+  $.post('/appointments', data, function () {
+    ajaxCalls.loadAll();
+  });
+};
+
+ajaxCalls.loadAll = function() {
+  $('#main').html('');
+  $.getJSON('/appointments', function (data) {
+    var result = { results: data };
+    _.each(data, function(item) {
+      $('#main').append(template(item)); 
+    }); 
+  });
+};
+
+ajaxCalls.updateItem = function (id, data) {
+    $.ajax({
+        type: 'PUT',
+        url: '/appointments/' + id,
+        data: data
+    }).done(function () {
+        stateEvents.loadList();
+    });
+};
+
+ajaxCalls.deleteItem = function (id) {
+    $.ajax({
+        type: 'DELETE',
+        url: '/appointments/' + id
+    }).done(function () {
+        stateEvents.loadList();
+    });
+};
+
 $(document).ready(function () {
+  template = _.template($("#template").html()); 
+  $('#addReservation').click( function() {
+    ajaxCalls.addNew($('#firstname').val(), $('#lastname').val(), null, "");
+  });
+
+  ajaxCalls.loadAll();
+});
+/*
     var Reservation = Backbone.Model.extend({
     });
 
@@ -53,4 +100,4 @@ $(document).ready(function () {
     pageview.collection.fetch().complete(pageview.fetchSuccess());
 
 
-});
+*/
